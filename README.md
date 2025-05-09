4267a48f538dd70ab2d37c7de99c932158cf2c6f
Michael Gastambides <michaelgastambides@gmail.com>
2025-05-09 16:44:29 -0400

    Started project, created database with MySQL, Created backend and frontend, added package.json, connected database to backend. npm start on backend and localhost:3000 to test connect to backend. localhost:3000/books to see the test database.

---
+++ README.md
@@ -0,0 +1,16 @@
+I am replicating a tutorial from youtube. 
+
+original video: https://www.youtube.com/watch?v=fPuLnzSjPLE
+
+I am building an fullstack app using Node.js, MySQL, and react. 
+
+We will fetch books from a database(MySQL)
+
+We can create a new book, delete a specific book or even update a book.
+
+Steps I took:
+
+I stared with downloading MySQL and createing a database called tests, to test the database,
+that creates a book with an id, title, description, and cover.png. 
+
+
---
+++ back-end/index.js
@@ -0,0 +1,30 @@
+import express from "express"
+import mysql from "mysql2"
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const app = express()
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+})
+
+app.get("/", (req,res)=>{
+res.json("hello this is the backend")
+})
+
+app.get("/books", (req,res)=>{
+    const q = "SELECT * FROM books"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.listen(3000, ()=>{
+    console.log("Connected to backend")
+})
